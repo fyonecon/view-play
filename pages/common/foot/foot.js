1a5b2b3401a046b6e6cfe7099d5c9a7066be7904
fyone-mac <oneint2652@outlook.com>
2019-02-26 15:30:04 +0800

    view web框架的一个应用例子。BBC、VOA在线广播应用。

---
+++ pages/common/foot/foot.js
@@ -0,0 +1,14 @@
+
+// 注射文件
+(function () {
+
+    const file_path = page_url + "common/foot/foot.htm";
+
+    view.write_htm(file_path, "depend", run_foot);
+
+})();
+
+function run_foot(e) {
+    view.log("foot渲染完成。");
+
+}
---
+++ pages/home/home.js
@@ -0,0 +1,14 @@
+/*
+* 局部模块js
+* */
+
+
+
+
+function start_this_page() {
+    view.log("主框架解析完成，开始渲染模块页面 > >");
+
+    window.location.replace("index.html#route=fm");
+}
+
+
